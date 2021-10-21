f6bfcc931a238c94a1be37fba9bb9c4c2241a459
AlbertoGR <albertogr578@gmail.com>
2021-10-21 12:41:06 +0200

    Añade la clase Ej11 y modifica la clase Ej10

--- src/main/java/estructurarepeticion/Ej10.java
+++ src/main/java/estructurarepeticion/Ej10.java
@@ -11,12 +11,21 @@ package estructurarepeticion;
 /*
 10.- Implementa un programa Java que permita visualizar todas las letras minúsculas, 
 desde la 'a' hasta la 'z', usando un for.
-*/
+ */
 public class Ej10 {
-    
+
     public static void main(String[] args) {
-        
-        
+
+        char primeraLetraAbecedario = 'a';
+
+        for (char z = 'z'; primeraLetraAbecedario <= z; primeraLetraAbecedario++) {
+
+            System.out.print(primeraLetraAbecedario);
+            System.out.print(" - ");
+
+        }
+
+        System.out.println(primeraLetraAbecedario);
     }
-    
+
 }
